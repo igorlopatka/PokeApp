a91d7be61ad6117063be4cc2c2be8664cf91559f
Igor Łopatka <igor.lopatka11@gmail.com>
2023-03-13 20:51:43 +0100

    Add PokeAPI protocol

---
+++ PokeApp/Service/PokeAPIProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  PokeAPIProtocol.swift
+//  PokeApp
+//
+//  Created by Igor Łopatka on 13/03/2023.
+//
+
+import Foundation
+
+protocol PokeAPIProtocol {
+    
+    func getPokemonList() -> AnyPublisher<[Pokemon], Error>
+    func getPokemonDetails(url: URL) -> AnyPublisher<PokemonDetails, Error>
+}
